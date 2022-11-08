e104bcf57662d82d510c6587e2055e6f2be3ab09
ShebangDog <twilight.sky.asterisk@gmail.com>
2022-11-08 09:04:08 +0900

    refactor: use pipe ope

--- lib/combinations.ex
+++ lib/combinations.ex
@@ -2,10 +2,8 @@ defmodule Combinations do
   @moduledoc false
 
   def of(set, pick) when length(set) >= pick do
-    tree = GenerateTree.of(set, pick)
-    list = ConvertTree.of(tree)
-
-    Enum.flat_map(list, &Enumerate.of/1)
+    GenerateTree.of(set, pick) |>
+    Enumerate.of
   end
 
 end
